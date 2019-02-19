4cad7bd2b4614fe8358aed33b45167e9063b5242
Geoff Gorham <geogor37@gmail.com>
2019-02-18 20:40:43 -0700

    Set up javascript to get a response from the server

--- script.js
+++ script.js
@@ -6,4 +6,13 @@ document.getElementById("input-submit").addEventListener("click", function (even
   const gender = maleButton.checked ? maleButton.value : femaleButton.value;
   console.log(birthDate);
   console.log(gender);
+
+  const url = `http://api.population.io:80/1.0/life-expectancy/total/${gender}/United%20States/${birthDate}/`;
+  console.log(url);
+  fetch(url)
+      .then(function (response) {
+        return response.json();
+      }).then(function (json) {
+    console.log(json);
+  });
 });
